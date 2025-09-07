cf92db788fc751753a6743ba9ccc6f6edfbcd80d
Alexandru Straton <straton.workk@gmail.com>
2025-09-07 11:05:49 +0300

    Spring Initializr

---
+++ src/main/java/com/yettensyvus/orarUSM/OrarUsmApplication.java
@@ -0,0 +1,13 @@
+package com.yettensyvus.orarUSM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrarUsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrarUsmApplication.class, args);
+	}
+
+}
